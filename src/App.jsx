982a28afb0813571e8fc11e01c2ae96e626284f0
RichrdYashiro <46631433+RichrdYashiro@users.noreply.github.com>
2025-06-11 01:23:56 +0300

    post, get, delete & put

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,68 @@ import "./App.css";
 function App() {
 	const [tasks, setTasks] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [refreshTasksFlag, setRefreshTasksFlag] = useState(false);
+	const [blockButton, setBlockButton] = useState(false);
+	const refreshTask = () => {
+		setRefreshTasksFlag((prev) => !prev);
+	};
 
 	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/todos/")
+		fetch("http://localhost:3000/todos")
 			.then((response) => response.json())
 			.then((loadTask) => {
 				setTasks(loadTask);
 				setIsLoading(false);
 			})
 			.catch((error) => console.error("Ошибка получения данных", error));
-	}, []);
+	}, [refreshTasksFlag]);
 
-	
+	const requestTask = () => {
+		setBlockButton(true);
+		fetch("http://localhost:3000/todos", {
+			method: "POST",
+			headers: { "Content-type": "application/json;charset=utf-8" },
+			body: JSON.stringify({
+				title: "твоя новая задача",
+			}),
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then(() => {
+				refreshTask();
+			})
+			.finally(() => setBlockButton(false));
+	};
+
+	const deliteTask = (id) => {
+		setBlockButton(true);
+		fetch(`http://localhost:3000/todos/${id}`, {
+			method: "DELETE",
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then(() => {
+				refreshTask();
+			})
+			.finally(() => setBlockButton(false));
+	};
+	const reworkBtn = (id) => {
+		setBlockButton(true);
+		fetch(`http://localhost:3000/todos/${id}`, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json;charset=utf-8" },
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then(() => {
+				refreshTask();
+			})
+			.finally(() => setBlockButton(false));
+	};
 	return (
 		<>
 			<h1>Список дела на React</h1>
+			<button onClick={requestTask} disabled={blockButton}>
+				Добавить задачу
+			</button>
+
 			{isLoading ? (
 				<div>Загрузка...</div>
 			) : (
@@ -28,6 +75,20 @@ function App() {
 						<div className="todo__item" key={id}>
 							<span>#{id}</span>
 							{title}
+							<button
+								className="reworkBtn"
+								onClick={() => reworkBtn(id)}
+								disabled={blockButton}
+							>
+								R
+							</button>
+							<button
+								className="delineBtn"
+								onClick={() => deliteTask(id)}
+								disabled={blockButton || tasks.length === 0}
+							>
+								X
+							</button>
 						</div>
 					))}
 				</div>
